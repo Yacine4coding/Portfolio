589394d0d4d547af7ed1157c52dd41c3da9f0b55
Yacine Bensidahmed <yacinebensidahmed.off@gmail.com>
2024-02-03 10:44:06 +0100

    Adding Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FEFFF0",
+        secondary: "#4A455C",
+        dim: "#878EA5",
+        dimBlue: "#403B43",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};
