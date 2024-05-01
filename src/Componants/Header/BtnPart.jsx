2d4c9534db5e0fd244984e8155ae8eaa92180060
Yacine Bensidahmed <yacinebensidahmed.off@gmail.com>
2024-05-01 21:36:15 +0100

    tsx transform to jsx + tailwind setup

---
+++ src/Componants/Header/BtnPart.jsx
@@ -0,0 +1,12 @@
+// import "../Style/Header.css"
+
+const BtnPart = () => {
+  return (
+    <div className="btn-part">
+      <span>See Portfolio</span>
+      <img src="src\assets\BtnEfct.svg" alt="" />
+    </div>
+  )
+}
+
+export default BtnPart
---
+++ src/Componants/Skills.jsx
@@ -0,0 +1,14 @@
+
+import "./Style/Skills.css"
+
+export const Skills = () => {
+  return (
+    <div className="skills-Contianer">
+      <span title="Beginner" >Figma</span>
+      <span title="Intermediate">HTML</span>
+      <span title="Intermediate">CSS</span>
+      <span title="Intermediate">JavaScript</span>
+      <span title="Intermediate">React.js/ts</span>
+    </div>
+  )
+}
