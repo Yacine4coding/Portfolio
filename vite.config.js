f5d33e5897439723ccc959bdc3f16cf1d9b9b946
Yacine Bensidahmed <yacinebensidahmed.off@gmail.com>
2024-05-30 16:42:49 +0100

    pnpm -> npm

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Specify the output directory
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
   },
 })
-
-
